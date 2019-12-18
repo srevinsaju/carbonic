e972d7b9574fe702c63d8f8d649c62cba6c64437
srevinsaju <srevin03@gmail.com>
2019-12-18 16:00:16 +0300

    Redit outdated LICENSE informations

--- ccore.py
+++ ccore.py
@@ -6,31 +6,20 @@ CARBONIC CORE AI
 CONVERTS IUPAC NAMES TO CARBON STRUCTURES
 SRC ON https://github.com/srevinsaju/carbonic
 
-ALL CODE IS LICENSED UNDER GNU-GPL LICENSE. READ LICENSE FOR MORE INFORMATION
+ALL CODE IS LICENSED UNDER GNU-AGPL LICENSE. READ LICENSE FOR MORE INFORMATION
 
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, version 3.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
 
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
 """
 # imports
 import os
@@ -47,7 +36,7 @@ CARBONIC CORE AI
 CONVERTS IUPAC NAMES TO CARBON STRUCTURES
 SRC ON https://github.com/srevinsaju/carbonic
 
-ALL CODE IS LICENSED UNDER GNU-GPL LICENSE. READ LICENSE FOR MORE INFORMATION
+ALL CODE IS LICENSED UNDER GNU-AGPL v3 LICENSE. READ LICENSE FOR MORE INFORMATION
 
 """)
 class carbox:
--- cli-engine.py
+++ cli-engine.py
@@ -1,3 +1,25 @@
+"""
+carbonic on github by @srevinsaju
+(c) 2019 by Srevin Saju (srevinsaju.github.io)
+CARBONIC COMMAND LINE INTERFACE
+CONVERTS IUPAC NAMES TO CARBON STRUCTURES
+SRC ON https://github.com/srevinsaju/carbonic
+
+ALL CODE IS LICENSED UNDER GNU-AGPL LICENSE. READ LICENSE FOR MORE INFORMATION
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from ccore import convertor, chkBond, displax
 
 iupac = input('Enter the IUPAC name of the carbon compound <x> ')
